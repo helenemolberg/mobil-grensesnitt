564a8c472ee163d31bb0075ebd0f5a590da203db
helenemolberg <helenesm@stud.ntnu.no>
2021-05-01 12:55:46 +0200

    fix

--- src/components/pages/Upload.js
+++ src/components/pages/Upload.js
@@ -49,7 +49,7 @@ const Opplastning = () => {
     userFile.imageName = imageFile.name;
 
     //Lagrer dato fra lastModifiedDate -> er den nøyaktig nok??
-    userFile.captureDate = format(new Date((imageFile.lastModifiedDate).replace(/\s/, 'T')), 'dd/MM/yyyy');
+    userFile.captureDate = new Date(imageFile.lastModifiedDate.replace(/-/g, '/'));
 
     //Sjekke om det finnes gps-verdier i filen
     let exifrOutput = await exifr.gps(imageFile).catch(console.error)
